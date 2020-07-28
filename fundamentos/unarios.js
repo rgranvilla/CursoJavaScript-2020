abd8ac5bdb3b2e984a817cf268dc8f2e4794d596
Ricardo Granvilla <rgranvilla@gmail.com>
2020-07-28 17:47:28 -0300

    Aula 48

---
+++ fundamentos/unarios.js
@@ -0,0 +1,17 @@
+let num1 = 1
+let num2 = 2
+
+num1++
+console.log(num1)
+--num1
+console.log(num1)
+
+console.log(++num1 === num2--)
+console.log(num1 === num2)
+
+let base = 2
+base **= 3 //Eleva o valor armazenado na base à terceira potência
+console.log(base) //2 elevado a 3 igual a 8
+
+let exp = 9 ** (1/2)
+console.log(exp)
